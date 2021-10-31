21826632b70cc08b96e0bd1b3aaf7eb701962ecf
James Prentice <jamesprenticez@gmail.com>
2021-10-31 19:07:52 +1300

    Polish to sign in page

--- pages/auth/signin.jsx
+++ pages/auth/signin.jsx
@@ -1,5 +1,5 @@
 import { getProviders, signIn as SignIntoProvider } from "next-auth/react"
-import Image from "next/image"
+import Link from "next/link"
 
 const logos = {
   GitHub: "/github.svg",
@@ -9,22 +9,23 @@ const logos = {
 
 //Client
 function signIn({ providers }) {
-  console.log(Object.values(providers))
   return (
     <>
-    <div className="p-10 h-screenHead flex flex-wrap justify-center items-center">
+    <div className="p-10 h-screenNav flex flex-wrap justify-center items-center">
       <div className="w-3/6 xl:w-1/5 border border-gray-600 rounded">
           <div className="flex flex-wrap justify-center h-1/4 w-full">
-            <div className="p-3 full-rounded">
-              <Image
+            <div className="p-4">
+            <Link href="/">
+              <img
                   src={'/icon.svg'}
-                  className="rounded-full"
+                  className="bg-black border-2 border-black rounded-full hover:cursor-pointer"
                   width="180"
                   height="180"
                   layout="intrinsic"
                   objectFit="contain"
                   alt=""
               />
+              </Link>
             </div>
           </div>
 
@@ -37,33 +38,33 @@ function signIn({ providers }) {
 
             <label className="text-black font-semibold">
               Username:
-              <input name="username" type="text" className="w-full mb-2 p-1 bg-transparent border border-gray-600 text-gray-600 rounded focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"/>
+              <input name="username" type="text" className="w-full mb-2 p-1 bg-transparent border border-gray-600 rounded focus:outline-none focus:ring-1 focus:ring-green-600 focus:border-transparent"/>
             </label>
            
             <label className="text-black font-semibold">
               Password:
-              <input name="password" type="password" className="w-full mb-6 p-1 bg-transparent border border-gray-600 text-gray-600 rounded focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"/>
+              <input name="password" type="password" className="w-full mb-6 p-1 bg-transparent border border-gray-600 rounded focus:outline-none focus:ring-1 focus:ring-green-600 focus:border-transparent"/>
             </label>
 
             <button 
               type="submit"
-              className="w-full border border-gray-600 bg-black hover:border-green-600 hover:text-green-600 text-base text-white p-2 rounded"
+              className="w-full border border-gray-600 bg-black text-base text-white p-2 rounded hover:border-green-600 hover:text-green-600 hover:scale-105 transform transition duration-200 ease-out"
             >
               Sign in
             </button>
           </form>
 
-          <div className="flex items-center">
-            <div className="h-px flex-1 bg-green-600"></div>
+          <div className="flex items-center justify-center">
+            <div className="ml-4 h-px flex-1 bg-green-600"></div>
             <p className="pl-2 pr-2">or</p>
-            <div className="h-px flex-1 bg-green-600"></div>
+            <div className="mr-4 h-px flex-1 bg-green-600"></div>
           </div>
 
           <div className="flex flex-wrap justify-center w-full p-4">
             {Object.values(providers).map((provider) => (
               <div key={provider.name} className="w-full">
                 <button 
-                  className="mt-2 p-2 border border-gray-600 rounded w-full text-center"
+                  className="mt-2 p-2 border border-gray-600 rounded w-full text-center hover:scale-105 transform transition duration-200 ease-out"
                   onClick={() => 
                     SignIntoProvider(provider.id, {callbackUrl: "/"})}
                   >
